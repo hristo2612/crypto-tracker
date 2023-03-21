b847270e8fc29b602445e217a7526b7fc5c9be50
hristo2612 <hristoapps@gmail.com>
2023-03-22 00:17:27 +0200

    Add Popover view
    When you click the icon it will open up this view

--- CryptoTracker/AppDelegate.swift
+++ CryptoTracker/AppDelegate.swift
@@ -18,7 +18,18 @@ extension AppDelegate {
         
         if let button = statusItem?.button {
             button.image = NSImage(systemSymbolName: "bitcoinsign.circle", accessibilityDescription: "Bitcoin")
+            button.action = #selector(handleClick)
         }
     }
+    
+    @objc func handleClick() {
+        let popover = NSPopover()
+        let view = NSHostingView(rootView: PopoverCoinView())
+        let hostingController = NSHostingController(rootView: PopoverCoinView())
+        popover.behavior = .transient
+        popover.contentViewController = hostingController
+        popover.contentViewController?.view = hostingController.view
+        popover.show(relativeTo: statusItem!.button!.bounds, of: statusItem!.button!, preferredEdge: .minY)
+        popover.contentSize = hostingController.view.fittingSize
+    }
 }
-
---
+++ CryptoTracker/PopoverCoinView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct PopoverCoinView: View {
+    var body: some View {
+        HStack {
+            Text("Hello, World!")
+                .font(.system(size: 12))
+        }
+        .padding()
+    }
+}
+
+struct PopoverCoinView_Previews: PreviewProvider {
+    static var previews: some View {
+        PopoverCoinView()
+    }
+}
