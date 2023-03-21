01c80c8d33e7069a1ea0e28a5628f381b0c28576
hristo2612 <hristoapps@gmail.com>
2023-03-21 19:53:58 +0200

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Hristo & Kristina on 21.03.23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
